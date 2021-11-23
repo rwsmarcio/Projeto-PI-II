b98c430ed3ae20b032da202e6068044c5aab75c7
Márcio Roberto Wilbert de Souza <rwsmarcio@gmail.com>
2021-11-23 18:10:22 -0300

    Primeira entrega

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const porta = 3000;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const livroRota = require('./rotas/livro_rota');
+app.use('/livros', livroRota);
+
+const usuarioRota = require('./rotas/usuario_rota');
+app.use('/usuarios', usuarioRota);
+
+const autorRota = require('./rotas/autor_rota');
+app.use('/autores', autorRota);
+
+const movimentacaoRota = require('./rotas/movimentacao_rota');
+app.use('/movimentacao', movimentacaoRota);
+
+app.listen(porta,() => 
+    console.log(`Iniciando o servidor na porta ${porta}`)
+);
